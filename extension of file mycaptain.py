125ca9c54b2b6062dae8432d9949af1eb93c973a
riddhigupt <97887137+riddhigupt@users.noreply.github.com>
2022-01-27 06:54:40 -0800

    Add files via upload

---
+++ extension of file mycaptain.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n=input("Input the Filename:")
+Input the Filename:
+=============== RESTART: C:/Users/sony/AppData/Local/Programs/Python/Python39/extension of file mycaptain.py ===============
+Input the Filename:abc.py
+The extension of the file: ' py '
+>>> 
